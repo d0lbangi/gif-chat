e05c3e22466cf1df6044adb6fed4e49fe025d25e
Jeongho Park <d0lbangi13@naver.com>
2023-10-31 17:24:16 +0900

    FEAT: add files related to views

--- socket.js
+++ socket.js
@@ -1,27 +1,27 @@
 const WebSocket = require('ws');
 
 module.exports = (server) => {
- const wss = new WebSocket.Server({ server });
- 
- wss.on('connection', (ws, req) => {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  console.log('새로운 클라이언트 접속', ip);
-  ws.on('message', (message) => {
-    console.log(message.toString());
-  });
+  const wss = new WebSocket.Server({ server });
 
-  ws.on('error', (error) => {
-    console.log(error);
-  });
+  wss.on('connection', (ws, req) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    console.log('새로운 클라이언트 접속', ip);
+    ws.on('message', (message) => {
+      console.log(message.toString());
+    });
 
-  ws.on('close', () => {
-    clearInterval(ws.interval);
-  });
+    ws.on('error', (error) => {
+      console.log(error);
+    });
 
-  ws.interval = setInterval(() => {
-    if (ws.readyState === ws.OPEN) {
-      ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-    }
-  }, 3000);
- });
+    ws.on('close', () => {
+      clearInterval(ws.interval);
+    });
+
+    ws.interval = setInterval(() => {
+      if (ws.readyState === ws.OPEN) {
+        ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
+      }
+    }, 3000);
+  });
 };
